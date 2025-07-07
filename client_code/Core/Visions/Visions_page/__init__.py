131a6ab11165258b72e43f334421315c132c19f6
lcraddock@nova-mentis.com <lcraddock@nova-mentis.com>
2025-07-07 16:00:03 +0000

    Created Core, Core.Dashboard, Core.HomePage, Core.Resources and 5 more; Edited Components.Sidebar and settings; Deleted HomePage, LoginPage, Pages, Pages.Dashboard and 4 more

--- client_code/Core/Visions/Visions_page/__init__.py
+++ client_code/Core/Visions/Visions_page/__init__.py
@@ -1,10 +1,10 @@
-from ._anvil_designer import VisionsTemplate
+from ._anvil_designer import Visions_pageTemplate
 from anvil import *
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
-class Visions(VisionsTemplate):
+class Visions_page(Visions_pageTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
