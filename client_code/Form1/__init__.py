d4ab90d90716262f20b8bd030648212687088abb
lcraddock@nova-mentis.com <lcraddock@nova-mentis.com>
2025-07-08 14:37:19 +0000

    Created home_page, Form2 and Form1; Edited settings; Deleted components, components.sidebar, core, core.dashboard and 6 more

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,12 +1,12 @@
-from ._anvil_designer import stepsTemplate
+from ._anvil_designer import Form1Template
 from anvil import *
-import anvil.users
 import anvil.server
+import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
-class steps(stepsTemplate):
+class Form1(Form1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
