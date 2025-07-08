4a2a855e47cffc579146596cc18a82009c0597b2
lcraddock@nova-mentis.com <lcraddock@nova-mentis.com>
2025-07-08 19:40:11 +0000

    Edited DashboardPage, HomePage, VisionsPage, auth and 2 more

--- client_code/HomePage/__init__.py
+++ client_code/HomePage/__init__.py
@@ -1,28 +1,23 @@
-#from ._anvil_designer import HomePageTemplate
+from ._anvil_designer import HomePageTemplate
 from anvil import *
 import anvil.server
 import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
-#from ..VisionsPage import VisionsPage
-#from ..DashboardPage import DashboardPage
+from .. import State
+from ..VisionsPage import VisionsPage
+from ..DashboardPage import DashboardPage
 
 class HomePage(HomePageTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    self.profile_btn.text = anvil.users.get_user()['email']
+    self.logged_in_text.text = "Logged in as " + anvil.users.get_user()['email']
+    self.content_panel.clear()
+    self.content_panel.add_component(DashboardPage())
 
     # Any code you write here will run before the form opens.
-
-  def visions_link_click(self, **event_args):
-    """This method is called when the link is clicked"""
-    pass
-
-  def dashboard_link_click(self, **event_args):
-    """This method is called when the link is clicked"""
-    pass
 
   def logout_btn_click(self, **event_args):
     """This method is called when the button is clicked"""
@@ -30,6 +25,14 @@ class HomePage(HomePageTemplate):
     open_form('Login')
     pass
 
-  def profile_btn_click(self, **event_args):
-    """This method is called when the button is clicked"""
+  def dashboard_link_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    self.content_panel.clear()
+    self.content_panel.add_component(DashboardPage())
+    pass
+
+  def visions_link_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    self.content_panel.clear()
+    self.content_panel.add_component(VisionsPage())
     pass
