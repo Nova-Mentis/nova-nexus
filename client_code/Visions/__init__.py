9452cab62b3493bae2843cff93a2bf4d6fa9b298
lcraddock@nova-mentis.com <lcraddock@nova-mentis.com>
2025-07-08 14:44:06 +0000

    Created HomePage, Visions and Dashboard; Edited settings; Deleted home_page, Form2 and Form1

--- client_code/Visions/__init__.py
+++ client_code/Visions/__init__.py
@@ -1,16 +1,14 @@
-from ._anvil_designer import home_pageTemplate
+from ._anvil_designer import VisionsTemplate
 from anvil import *
-import anvil.users
 import anvil.server
+import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
-class home_page(home_pageTemplate):
+class Visions(VisionsTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    anvil.users.login_with_form()
-    anvil.server.call('print_my_permissions')
 
     # Any code you write here will run before the form opens.
