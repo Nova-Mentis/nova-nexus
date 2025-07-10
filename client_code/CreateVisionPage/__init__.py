4feb4c25299343322d8394bc680869465e671402
lcraddock@nova-mentis.com <lcraddock@nova-mentis.com>
2025-07-10 21:22:06 +0000

    Edited Vision Creation

--- client_code/CreateVisionPage/__init__.py
+++ client_code/CreateVisionPage/__init__.py
@@ -7,6 +7,8 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 from .. import State
 
+vision_guidance_level = ""
+
 class CreateVisionPage(CreateVisionPageTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
@@ -17,16 +19,42 @@ class CreateVisionPage(CreateVisionPageTemplate):
     self.vision_type_dropdown.items = anvil.server.call('get_vision_types')
     self.questions_panel.items = anvil.server.call('get_questions_for_vision_type', self.vision_type_dropdown.selected_value)
 
+  def set_panel_visibility(self):
+    self.type_column_panel.visible = True
+    self.guided_questions_panel.visible = False
+    self.rough_idea_panel.visible = False
+    self.custom_vision_statement_panel.visible = False
+    if self.vision_guidance_level == "Guided":
+      self.guided_questions_panel.visible = True
+      self.rough_idea_panel.visible = False
+      self.custom_vision_statement_panel.visible = False
+    elif self.vision_guidance_level == "Assisted":
+      self.guided_questions_panel.visible = False
+      self.rough_idea_panel.visible = True
+      self.custom_vision_statement_panel.visible = False
+    else:
+      self.guided_questions_panel.visible = False
+      self.rough_idea_panel.visible = False
+      self.custom_vision_statement_panel.visible = True
+    pass
+    
+
   def guided_vision_btn_click(self, **event_args):
     """This method is called when the button is clicked"""
-    self.type_column_panel.visible = True
-    self.custom_vision_statement_panel.visible = False
+    self.vision_guidance_level = "Guided"
+    self.set_panel_visibility()
     pass
 
   def custom_vision_btn_click(self, **event_args):
     """This method is called when the button is clicked"""
-    self.type_column_panel.visible = True
-    self.custom_vision_statement_panel.visible = True
+    self.vision_guidance_level = "Custom"
+    self.set_panel_visibility()
+    pass
+
+  def rough_idea_btn_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    self.vision_guidance_level = "Assisted"
+    self.set_panel_visibility()
     pass
 
   def create_custom_vision_btn_click(self, **event_args):
@@ -44,6 +72,10 @@ class CreateVisionPage(CreateVisionPageTemplate):
   def vision_type_dropdown_change(self, **event_args):
     """This method is called when an item is selected"""
     self.questions_panel.items = anvil.server.call('get_questions_for_vision_type', self.vision_type_dropdown.selected_value)
-    self.guided_questions_panel.visible = True
     pass
+
+
+
+
+      
     
