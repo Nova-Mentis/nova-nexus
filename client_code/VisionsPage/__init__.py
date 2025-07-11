b6d063ac3d70c4b95fb2f25106ddaf7e15e566a8
lcraddock@nova-mentis.com <lcraddock@nova-mentis.com>
2025-07-11 08:14:02 +0000

    Edited VisionsPage

--- client_code/VisionsPage/__init__.py
+++ client_code/VisionsPage/__init__.py
@@ -12,11 +12,16 @@ class VisionsPage(VisionsPageTemplate):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
     self.vision_repeating_panel.items = anvil.server.call('get_visions_list', user=anvil.users.get_user(), tenant=State.tenant)
-    
+    self.vision_btn.add_event_handler('x-create-vision', self.handle_create_vision)
 
     # Any code you write here will run before the form opens.
 
+
+  def handle_create_vision(self, **event_args):
+    print("Create Vision event raised")
+  
   def vision_btn_click(self, **event_args):
     """This method is called when the button is clicked"""
+    self.vision_btn.raise_event('x-create-vision')
     open_form('CreateVisionPage')
     pass
