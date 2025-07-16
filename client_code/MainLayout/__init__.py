df11f6ca4c84f61755fa1b0a9765f883626cfd92
lcraddock@nova-mentis.com <lcraddock@nova-mentis.com>
2025-07-16 20:57:41 +0000

    Edited MainLayout

--- client_code/MainLayout/__init__.py
+++ client_code/MainLayout/__init__.py
@@ -7,46 +7,48 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 from ..VisionsPage import VisionsPage
 from ..DashboardPage import DashboardPage
+from anvil.designer import in_designer
 
 class MainLayout(MainLayoutTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    if not in_designer:
+      try:
+        anvil.server.call('server_heartbeat')
+      except anvil.server.SessionExpiredError:
+        anvil.server.reset_session()
     
-    try:
-      anvil.server.call('server_heartbeat')
-    except anvil.server.SessionExpiredError:
-      anvil.server.reset_session()
+      # Set Tenant
+      tenant_list = anvil.server.call("get_tenants")
+      self.tenant_dropdown.items = tenant_list
+      if anvil.server.call('get_cookies_tenant') is not None:
+        self.tenant_dropdown.selected_value = anvil.server.call('get_cookies_tenant')
+      else:
+        anvil.server.call('switch_session_tenant', self.tenant_dropdown.selected_value)
   
-    # Set Tenant
-    tenant_list = anvil.server.call("get_tenants")
-    self.tenant_dropdown.items = tenant_list
-    if anvil.server.call('get_cookies_tenant') is not None:
-      self.tenant_dropdown.selected_value = anvil.server.call('get_cookies_tenant')
-    else:
-      anvil.server.call('switch_session_tenant', self.tenant_dropdown.selected_value)
-
-    # Set Visibility for Admins
-    current_user_role = anvil.users.get_user()['role']['role_name']
-    print("User role is " + current_user_role)
-    if current_user_role == "super_admin":
-      self.tenant_dropdown.visible = True
-      self.user_link.visible = True
-      self.system_link.visible = True
-      self.tenant_manager_link.visible = True
-      self.resources_link.visible = True
-    elif current_user_role == "admin":
-      self.tenant_dropdown.visible = False
-      self.user_link.visible = True
-      self.system_link.visible = True
-      self.tenant_manager_link.visible = False
-      self.resources_link.visible = True
-    else:
-      self.tenant_dropdown.visible = False
-      self.user_link.visible = False
-      self.system_link.visible = False
-      self.tenant_manager_link.visible = False
-      self.resources_link.visible = False
+      # Set Visibility for Admins
+  
+      current_user_role = anvil.users.get_user()['role']['role_name']
+      print("User role is " + current_user_role)
+      if current_user_role == "super_admin":
+        self.tenant_dropdown.visible = True
+        self.user_link.visible = True
+        self.system_link.visible = True
+        self.tenant_manager_link.visible = True
+        self.resources_link.visible = True
+      elif current_user_role == "admin":
+        self.tenant_dropdown.visible = False
+        self.user_link.visible = True
+        self.system_link.visible = True
+        self.tenant_manager_link.visible = False
+        self.resources_link.visible = True
+      else:
+        self.tenant_dropdown.visible = False
+        self.user_link.visible = False
+        self.system_link.visible = False
+        self.tenant_manager_link.visible = False
+        self.resources_link.visible = False
 
 
     # Any code you write here will run before the form opens.
