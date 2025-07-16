752349d43ec13799fe7b49438592ef9d331726ac
lcraddock@nova-mentis.com <lcraddock@nova-mentis.com>
2025-07-16 20:39:20 +0000

    Edited EvaluateVisionPage

--- client_code/EvaluateVisionPage/__init__.py
+++ client_code/EvaluateVisionPage/__init__.py
@@ -5,9 +5,7 @@ import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
-from anvil.designer import in_designer
-if in_designer:
-  anvil.server.reset_session()
+
 
 class EvaluateVisionPage(EvaluateVisionPageTemplate):
   def __init__(self, **properties):
